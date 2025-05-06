08044838b4aacb6dacfc1d2716ae23c6a72c1b44
김택윤 <gw9122@Naver.com>
2025-05-06 17:52:36 +0900

    빌드 그래들 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	id("org.springframework.boot") version "3.3.11"
 	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("plugin.jpa") version "1.9.25"
+	kotlin("kapt") version "1.9.25" // ✨ 이 부분 추가!
 }
 
 group = "com.taxijjang"
@@ -27,13 +28,16 @@ dependencyManagement {
 }
 
 dependencies {
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta") // jakarta 버전 사용
+	kapt("com.querydsl:querydsl-apt:5.0.0:jakarta") // Q 클래스 생성용
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 
